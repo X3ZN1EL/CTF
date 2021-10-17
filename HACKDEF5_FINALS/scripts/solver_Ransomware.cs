518968407f5d6d7d3d58e59658b62ad9448aef7b
Alejandro Jacobo <72319598+X3ZN1EL@users.noreply.github.com>
2021-10-17 16:57:17 -0500

    Update solver_Ransomware.cs

--- HACKDEF5_FINALS/scripts/solver_Ransomware.cs
+++ HACKDEF5_FINALS/scripts/solver_Ransomware.cs
@@ -1,25 +1,17 @@
-
 /* 
 DarkSide
 leax was here 
-*/  
+*/
 
 using System;
-using System.Diagnostics;
-using System.Drawing;
-using System.IO;
 using System.Linq;
-using System.Runtime.InteropServices;
 using System.Security.Cryptography;
-using System.Security.Principal;
 using System.Text;
-using System.Windows.Forms;
-using Microsoft.Win32;
 
 public class Program
 {
     public static void Main()
-    {    
+    {
         string[] codes = new string[]
         {
             "aSsEBGLIOv6L7lAooXDnJJK/6dU=",
@@ -28,25 +20,35 @@ public class Program
             "AB8kPpDrXnEICQr0qRN1VcaOs1E=",
             "6nxLEfibzE+vN+/KoQTXTPRD8X0="
         };
-        foreach (int value in Enumerable.Range(0, 200))
-        {
-            string code = "GUHFJK";
-            char c = (char)value;
-            code += c.ToString();
-            byte[] bytes = Encoding.UTF8.GetBytes(code);
-            string text = Convert.ToBase64String(new SHA1CryptoServiceProvider().ComputeHash(bytes));
-            if (!Enumerable.Contains<string>(codes, text)){
-                if (text == "nYGgAcX2+YSNlfpg9/1OyNduNB8=")
+        bool flag = false;
+        string code = "";//GUHFJK
+        string password = "";
+        while (!flag){
+            foreach (int value in Enumerable.Range(0, 200))
+            {
+                char c = (char)value;
+                code += password + c.ToString();
+                byte[] bytes = Encoding.UTF8.GetBytes(code);
+                string text = Convert.ToBase64String(new SHA1CryptoServiceProvider().ComputeHash(bytes));
+                if (!Enumerable.Contains<string>(codes, text))
                 {
-                    Console.WriteLine(text);
-                    Console.WriteLine(code);
+                    if (text == "nYGgAcX2+YSNlfpg9/1OyNduNB8=" || password == "nYGgAcX2+YSNlfpg9/1OyNduNB8=")
+                    {
+                        Console.WriteLine("Cracked: "+ code);
+                        flag = true;
+                    }
+                    text = "";
+                    code = "";
                 }
-                Console.WriteLine("Vaciando Code");
-                code = "";
-                text="";
-            }
-            Console.WriteLine(text);
-            Console.WriteLine(code);
+                else
+                {
+                    Console.WriteLine(password);
+                    password = password + c.ToString();
+                    text = "";
+                    code = "";
+                }
+                
+            } 
         }
     }
 }
